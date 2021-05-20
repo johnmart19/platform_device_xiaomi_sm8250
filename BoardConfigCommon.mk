e1a829344cf942b7958a9040c0f19f8918d0f1cd
DennySPb <dennyspb@gmail.com>
2021-05-20 14:55:57 +0300

    BoardConfig: switch to Proton clang13 for kernel building
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -18,6 +18,8 @@ BOARD_VENDOR := xiaomi
 BUILD_BROKEN_DUP_RULES := true
 COMMON_PATH := device/xiaomi/sm8250-common
 
+ANDROID_TOP := $(shell pwd)
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a
@@ -46,7 +48,11 @@ BOARD_RAMDISK_OFFSET       := 0x02000000
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
-BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := 11
+KERNEL_TOOLCHAIN := $(ANDROID_TOP)/prebuilts/clang/host/linux-x86/clang-11/bin
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-gnu-
 BOARD_BOOT_HEADER_VERSION := 3
 VENDOR_CMDLINE := "console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket reboot=panic_warm androidboot.init_fatal_reboot_target=recovery"
 TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc
