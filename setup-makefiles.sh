63663a48c8d1b0af05489e04aab7cf5b35a1f18d
DennySPb <dennyspb@gmail.com>
2021-06-01 16:03:00 +0300

    sm8250-common: Add vendor blobs extraction script
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2021 Syberia Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -10,15 +11,15 @@ set -e
 DEVICE_COMMON=sm8250-common
 VENDOR=xiaomi
 
-INITIAL_COPYRIGHT_YEAR=2020
+INITIAL_COPYRIGHT_YEAR=2021
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="${MY_DIR}/../../.."
+SYBERIA_ROOT="${MY_DIR}/../../.."
 
-HELPER="${LINEAGE_ROOT}/vendor/syberia/build/tools/extract_utils.sh"
+HELPER="${SYBERIA_ROOT}/vendor/syberia/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -26,7 +27,7 @@ fi
 source "${HELPER}"
 
 # Initialize the helper for common
-setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true
+setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${SYBERIA_ROOT}" true
 
 # Copyright headers and guards
 write_headers "alioth thyme"
@@ -34,17 +35,5 @@ write_headers "alioth thyme"
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
-if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
-    # Reinitialize the helper for device
-    INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
-    setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false
-
-    # Copyright headers and guards
-    write_headers
-
-    # The standard device blobs
-    write_makefiles "${MY_DIR}/../${DEVICE}/proprietary-files.txt" true
-
-    # Finish
-    write_footers
-fi
+# We are done!
+write_footers
