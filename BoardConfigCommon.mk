5738ab02ee56690584801a75740514bf83b5ff94
Arne Coucheron <arco68@gmail.com>
2021-06-13 12:20:47 +0300

    sm8250-common: Remove obsolete TARGET_BOARD_PLATFORM_GPU
    Signed-off-by: mdeejay <mdjrussia@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -163,7 +163,6 @@ TARGET_COPY_OUT_VENDOR := vendor
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := kona
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno650
 
 # Properties
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
